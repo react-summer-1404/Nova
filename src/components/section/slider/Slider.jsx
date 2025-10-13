b2e2d81cebd365c2e395a7a2508c0567b896d3b6
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-10-13 13:39:05 +0330

    slider style is done

--- src/components/section/slider/Slider.jsx
+++ src/components/section/slider/Slider.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import sliderData from "./sliderData"
 import "../../../assets/styles/global.css"
+import "../../../assets/styles/variable.css"
 import useSlider from "../../../hooks/useSlider"
+import YellowButton from '../../ui/button/YellowButton'
+import { GoArrowLeft, GoArrowRight } from "react-icons/go";
 
 const Slider = () => {
   const itemsPerPage = 6;
@@ -11,23 +14,25 @@ const Slider = () => {
   });
   
   return (
-    <div className="bg-gray-500 flex-center md:w-3/4  p-2 rounded-full w-[400px]">
+    <div style={{backgroundColor: "var(--color-light-gray)"}} className=" flex-center md:w-3/4  p-2 rounded-full w-[400px]">
       
-      <button className=" p-4 rounded-full mr-5" onClick={nextSlide}>pre</button>
+      <YellowButton onClick={prevSlide}  width={"50px"} height={"50px"} icon={<GoArrowLeft className='-m-1'/>}/>
   
       <div className="overflow-hidden">
-        <div className="flex w-fit gap-1 " style={{ transform: `translateX(-${slide * (100 / itemsPerPage)}%)`} } >
+        <div className="flex w-fit gap-1 transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${slide * (100 / itemsPerPage)}%)`} } >
           {sliderData.map((item, index) => (
             <div key={index} className="flex flex-col  items-center simple-border w-1/6 flex-shrink-0">
             <img src={item.pic} alt={item.title} className="w-2/3" />
-          <h3>{item.title}</h3>
-          <span>{item.totalNumber}</span>
+          <h3 className='font-medium text-responsive'>{item.title}</h3>
+          <span style={{color:("var(--color-text-gray)")}}>{`(${item.totalNumber})`}</span>
             </div>
           ))}
         </div>
       </div>
   
-      <button className="bg-blue-500 p-4 rounded-full ml-5" onClick={prevSlide}>next</button>
+      <YellowButton onClick={nextSlide}  width={"50px"} height={"50px"} icon={<GoArrowRight className='-m-1'/>}/>
+
+
     </div>
   
 
--- src/components/ui/button/YellowButton.jsx
+++ src/components/ui/button/YellowButton.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-const YellowButton = ({ width, text, icon }) => {
+const YellowButton = ({ width,height, text, icon ,onClick}) => {
   return (
     <button
       type="submit"
       style={{
         width,
+        height,
         backgroundColor: "var(--color-golden-yellow)",
         fontFamily: "var(--font-base)",
       }}
-      className="border border-black rounded-[50px] p-5 shadow-2d-yellow text-[20px] font-semibold leading-[18px] text-center cursor-pointer"
+      className="border border-black rounded-[50px] p-5 shadow-2d-yellow text-[20px] font-semibold leading-[18px] text-center cursor-pointer "
+    onClick={onClick}
     >
       {text}
+      {icon&& <span >{icon}</span>}
     </button>
   );
 };
