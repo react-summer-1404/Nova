863270f73ab752fd53b3fa9d7d760402718fd561
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2025-10-17 15:58:17 +0330

    chen2

--- src/components/section/landing/BlogSection/BlogSection.jsx
+++ src/components/section/landing/BlogSection/BlogSection.jsx
@@ -8,7 +8,7 @@ const BlogSection = () => {
   return (
     <div style={{backgroundColor: "var(--color-light-gray)"}} className ='w-screen h-[680px] flex justify-center items-center '>
       <div className ='w-[80%]  h-[70%] flex flex-col items-center gap-3'>
-        <button className ='w-[9%] h-[25px] rounded-[30px] bg-[#EFEEFE] text-[9px] text-[#5751E1] '>
+        <button className ='p-1 rounded-[30px] bg-[#EFEEFE] text-[9px] text-[#5751E1] px-6'>
         اخبار و وبلاگ ها
         </button>
         <p style={{color: "var(--color-navy)"}} className ='font-[600] text-[28px]'>آخرین خبر ما</p>
