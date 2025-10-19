43bbeee6ba8c0f66e9e0498475bcb7e71fd38440
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-10-19 18:02:25 +0330

    interceptor & env file & react quary

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 import { HeroUIProvider } from "@heroui/react";   
-
+import {  QueryClient, QueryClientProvider} from "@tanstack/react-query"
 const App = () => {
+  const client =new QueryClient ()
   return (
+    <QueryClientProvider client={client}>
     <HeroUIProvider>
       <RouterProvider router={router} />
     </HeroUIProvider>
+    </QueryClientProvider>
   );
 };
 
--- src/core/interceptor/interceptor.js
+++ src/core/interceptor/interceptor.js
@@ -7,7 +7,7 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use((config) => {
-  const token = getToken;
+  const token = getToken();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
@@ -29,3 +29,4 @@ instance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+export default instance;
