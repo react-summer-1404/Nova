ce4220c753394bf691b8a51fe4a5bb3ef687f845
aminasama <236314759+aminasamacreator-byte@users.noreply.github.com>
2025-10-22 00:35:39 -0700

    new update login

--- src/servises/api/auth/login/index.js
+++ src/servises/api/auth/login/index.js
@@ -10,16 +10,3 @@ export const login = async ({ phoneOrGmail, password, rememberMe }) => {
   return response.data;
 };
 
-// async function LoginApi(params) {
-//   const response = await axios.post(
-//     "https://sepehracademy.liara.run/Sign/Login",
-//     {
-//       phoneOrGmail: "",
-//       password: params.password,
-//       rememberMe: true,
-//     }
-//   );
-//   return response;
-// }
-
-// export default LoginApi;
