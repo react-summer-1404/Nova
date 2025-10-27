1810f459a7007f29765507b2a117cd1296096f13
aminasama <236314759+aminasamacreator-byte@users.noreply.github.com>
2025-10-27 02:30:41 -0700

    add hero ui kit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ const {heroui} = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js",
-  ],
+    "./node_modules/@heroui/theme/dist/components/(autocomplete|button|ripple|spinner|form|input|listbox|divider|popover|scroll-shadow).js"
+],
   theme: {
     extend: {},
   },
