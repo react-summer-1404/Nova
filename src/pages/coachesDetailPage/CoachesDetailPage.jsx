79bdc5232ecdd6348aa1c1e6becd7f5513b963bf
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-11-03 18:05:15 +0330

    navigation added

--- src/pages/coachesDetailPage/CoachesDetailPage.jsx
+++ src/pages/coachesDetailPage/CoachesDetailPage.jsx
@@ -6,7 +6,7 @@ import { getTeachersDetail } from "../../servises/api/teacherDetail";
 import TeacherInformation from "../../components/section/teacherDetail/TeacherInformation/TeacherInformation";
 import FilterTeacherCourses from "../../components/section/teacherDetail/FilterTeacherCourses/FilterTeacherCourses";
 import ContactTeacher from "../../components/section/teacherDetail/ContactTeacher/ContactTeacher";
-
+import NavigationSection from "../../components/ui/navigation/NavigationSection";
 const CoachesDetailPage = () => {
   // const {teacherId} =useParams();
   const TeacherId = 1;
@@ -16,13 +16,17 @@ const CoachesDetailPage = () => {
   });
 
   return (
-    <div className="w-screen flex justify-center gap-[20px] ">
+   <div className="flex flex-col">
+        <NavigationSection title={teacherDetail.fullName}/>
+
+     <div className="w-screen flex justify-center gap-[20px] py-[90px]">
       <div className="flex flex-col md:w-[80%] lg:w-[60%] w-4/5 items-end gap-15">
         <TeacherInformation teacherDetail={teacherDetail} />
         <FilterTeacherCourses TeacherId={TeacherId} />
       </div>
      <ContactTeacher/>
     </div>
+   </div>
   );
 };
 
