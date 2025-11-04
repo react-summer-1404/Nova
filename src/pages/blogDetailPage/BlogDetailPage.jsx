3fb6bf85f59b4fd2f8722b363f3268dd1e0e6334
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-11-04 10:40:12 +0330

    ...

--- src/pages/blogDetailPage/BlogDetailPage.jsx
+++ src/pages/blogDetailPage/BlogDetailPage.jsx
@@ -1,12 +1,26 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import { getBlogsDetail } from '../../servises/api/newsDetail'
+import Tag from '../../components/ui/Tag/Tag';
+import "../../assets/styles/"
 
 const BlogDetailPage = () => {
+ const id=1;
+  const {data}= useQuery({
+    queryKey:["newsDetail",id],
+    queryFn: ()=> getBlogsDetail(id)
+  })
+  console.log(data)
+  const detailItems = data?.detailsNewsDto;
   return (
 <div className='flex w-screen justify-center'>
-<div className='flex'>
-     <div className='flex flex-col w-4/5 border'>
-      <img className='w-full'/>
-      <div>info</div>
+<div className='flex w-[80%]'>
+     <div className='flex flex-col w-4/5 border '>
+      <img src={detailItems?.currentImageAddress} className='w-full h-[500px] '/>
+      <div>
+        <div><Tag bgColor={"var()"}/></div>
+        <div></div>
+      </div>
       <div>details</div>
       <div>comment</div>
      </div>
