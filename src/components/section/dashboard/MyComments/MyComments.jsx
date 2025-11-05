fd2a952a8e53dcc14fe3da954f2384d3cbaf4b41
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2025-11-05 20:59:18 +0330

    first comit

--- src/components/section/dashboard/MyComments/MyComments.jsx
+++ src/components/section/dashboard/MyComments/MyComments.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import CourseTableRow from './CourseTableRow/CourseTableRow'
 
 const MyComments = () => {
   return (
-    <div className ='border w-full h-[400px] flex items-center justify-center flex-col'>
-      <div className ='w-full h-[34px] border'>
+    <div className =' w-full flex items-center justify-center flex-col border '>
+      <div className ='w-[80%] border'>
+        <CourseTableRow 
+          items={[
+            "زمان",
+            "وضعیت",
+            "دسته بندی",
+            "دیدگاه در",            
+          ]}
+        />
+      </div>
+      <div className ='h-1 w-full bg-gradient-to-r from-transparent via-golden-yellow to-transparent'></div>
+      <div className ='w-full h-[100px] mt-[12px] border'>
 
       </div>
     </div>
