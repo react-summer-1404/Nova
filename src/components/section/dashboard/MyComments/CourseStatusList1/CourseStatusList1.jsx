167b687ae3108a1f651d101ed39bae1bc3cefc6b
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2025-11-06 03:32:18 +0330

    point

---
+++ src/components/section/dashboard/MyComments/CourseStatusList1/CourseStatusList1.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+const CourseStatusList1 = ({items1}) => {
+    return (
+        <div className='grid grid-flow-col auto-cols-fr py-2 text-[12px] lg:text-[14px] font[600] text-navy bg-[#C8C1ED4D] rounded-[5px] shadow-[0px_1px_10px_0px_rgba(0,0,0,0.25)]'>
+            {items1.map((item, index) =>(
+                <div key={index} className ={`text-center mr-[10px] flex flex-col items-center gap-1 ${item.className || ""}`}>
+                    {item.image && (
+                    <img
+                        src={item.image}
+                        alt=''
+                        className ='w-8 h-8 rounded-full object-cover'
+                    />
+                    )}
+                    {item.icon && (
+                        <span>{item.icon}</span>
+                    )
+                    }
+                <span>{item.text}</span>
+                </div>                               
+            ))}
+        </div>
+    )
+}
+
+export default CourseStatusList1
--- src/components/section/dashboard/MyComments/MyComments.jsx
+++ src/components/section/dashboard/MyComments/MyComments.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import CourseTableRow from './CourseTableRow/CourseTableRow'
+import React from 'react';
+import CourseTableRow from './CourseTableRow/CourseTableRow';
+import CourseStatusList1 from './CourseStatusList1/CourseStatusList1';
+import { HiOutlineTrash } from "react-icons/hi2";
+import { IoEyeOutline } from "react-icons/io5";
+import CourseStatusList2 from './CourseStatusList2/CourseStatusList2';
+
 
 const MyComments = () => {
   return (
-    <div className =' w-full flex items-center justify-center flex-col border '>
-      <div className ='w-[80%] border'>
+    <div className =' w-full flex items-center justify-center flex-col'>
+      <div className ='w-[80%] '>
         <CourseTableRow 
           items={[
             "زمان",
@@ -15,8 +20,77 @@ const MyComments = () => {
         />
       </div>
       <div className ='h-1 w-full bg-gradient-to-r from-transparent via-golden-yellow to-transparent'></div>
-      <div className ='w-full h-[100px] mt-[12px] border'>
+      <div className ='w-full mt-[12px] gap-3 flex flex-col'>
 
+        <CourseStatusList1
+          items1={[
+            {
+              icon : <div className ='flex gap-2 text-dark-purple'><HiOutlineTrash size={18}/> <IoEyeOutline size={18} /></div>
+            },
+            {
+              text : "۱۴۰۴/۰۳/۰۲ , ۱۶:۲۷",
+            },
+            {
+              text : "در انتظار تایید",
+              className : "text-[#DE5204]"
+            },
+            {
+              text: "دوره آموزشی"
+            },
+            {
+              text: "آموزش Tailwind css"
+            },
+            {
+              text: "#۱"
+            }
+          ]}
+        />
+        <CourseStatusList2
+          items2={[
+            {
+              icon : <div className ='flex gap-2 text-dark-purple'><HiOutlineTrash size={18}/> <IoEyeOutline size={18} /></div>
+            },
+            {
+              text : "۱۴۰۴/۰۲/۰۸ , ۱۵:۲۸",
+            },
+            {
+              text : "تایید  شده",
+              className : "text-[#005A34]"
+            },
+            {
+              text : "اخبار و مقالات",
+            },
+            {
+              text : "آموزش کامل کار با Figma",
+            },
+            {
+              text : "#۲",
+            }
+          ]}
+        />
+        <CourseStatusList1
+          items1={[
+            {
+              icon : <div className ='flex gap-2 text-dark-purple'><HiOutlineTrash size={18}/> <IoEyeOutline size={18} /></div>
+            },
+            {
+              text : "۱۴۰۴/۰۳/۰۲ , ۱۶:۲۷",
+            },
+            {
+              text : "در انتظار تایید",
+              className : "text-[#DE5204]"
+            },
+            {
+              text: "دوره آموزشی"
+            },
+            {
+              text: "آموزش NextJs"
+            },
+            {
+              text: "#۳"
+            }
+          ]}
+        />
       </div>
     </div>
   )
