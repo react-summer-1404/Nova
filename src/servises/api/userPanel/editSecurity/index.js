03d3378042237633f201b6af65837a5a77277612
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-11-07 20:36:23 +0330

    Update index.js

--- src/servises/api/userPanel/editSecurity/index.js
+++ src/servises/api/userPanel/editSecurity/index.js
@@ -2,7 +2,8 @@ import instance from "../../../../core/interceptor/interceptor";
 
 export const putSecurityEdit = async (apiData) => {
   const response = await instance.put("/SharePanel/EditSecurity", {
-    apiData,
+    twoStepAuth: apiData.twoStepAuth,
+    recoveryEmail: apiData.recoveryEmail,
     baseUrl: "http://localhost:5173/dashboard/securitysetting",
   });
   return response.data;
