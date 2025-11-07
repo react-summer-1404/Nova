eb32c8edfb6bb56f10005603c485f2301efda029
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-11-07 03:38:30 +0330

    user profile api called

--- src/components/section/dashboard/UserInformation/EditUserInformation/EditUserInformation.jsx
+++ src/components/section/dashboard/UserInformation/EditUserInformation/EditUserInformation.jsx
@@ -5,44 +5,34 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { getCurrentUserProfile } from "../../../../../servises/api/userPanel/getProfileInfo";
 import Subject from "./Subject/Subject";
 import { putEditProfile } from "../../../../../servises/api/userPanel/updateProfileInfo";
-import { HiOutlineCamera } from "react-icons/hi2";
-import profPic from "../../../../../assets/icons/ax-kartoni-bamazeh-11.svg";
-import profPicccccccc from "../../../../../assets/images/prof.png";
+
 import { postUserImage } from "../../../../../servises/api/userPanel/addProfileImage";
-import ModalSection from "../../../../ui/Modal/ModalSection";
-import { FaCheck } from "react-icons/fa";
-import { FaPlus } from "react-icons/fa";
+
 import useToggle from "../../../../../hooks/useToggle";
+import toast from "react-hot-toast";
+import ImageContainer from "./ImageContainer/ImageContainer";
 
 const EditUserInformation = () => {
-  const [isModalOpen , toggleModal,setIsModalOpen] = useToggle(false);
-  console.log(isModalOpen)
   const { data: currentProf } = useQuery({
     queryKey: ["currentProfUser"],
     queryFn: async () => await getCurrentUserProfile(),
   });
 
-  console.log("currentProf ==>", currentProf);
 
   const mutation = useMutation({
     mutationFn: (formData) => putEditProfile(formData),
     onError: (error) => {
       console.log("خطا رخ داد", error);
+      toast.error("خطا در ویرایش اطلاعات ! لطفا دوباره تلاش کنید")
+
     },
     onSuccess: () => {
-      console.log("ویرایش شد");
+      toast.success("پروفایل شما اپدیت شد")
     },
   });
 
-  const mutationImage = useMutation({
-    mutationFn: (formData) => postUserImage(formData),
-    onError: (error) => {
-      console.log("خطا رخ داد", error);
-    },
-    onSuccess: () => {
-      console.log("اپلود شد");
-    },
-  });
+ 
+  
 
   const submitHandling = async (values) => {
     try {
@@ -56,7 +46,6 @@ const EditUserInformation = () => {
       formData.append("UserAbout", values.UserAbout);
       formData.append("HomeAdderess", values.HomeAdderess);
       formData.append("Gender", values.Gender);
-      formData.append("formFile",values.formFile);
 
       // formData.append("Latitude", values.Latitude);
       // formData.append("Longitude", values.Longitude);
@@ -189,61 +178,7 @@ const EditUserInformation = () => {
               </button>
             </div>
             <div className=" flex flex-col border-1">
-              <div className=" relative overflow-hidden  rounded-full w-[150px] h-[150px]">
-                <img src={currentProf?.userPicture ||profPicccccccc }  alt="profile" />
-
-                <ModalSection
-                  StyleModal={
-                    "h-[30px] w-full bg-[#0E0E0E66] absolute bottom-0 flex justify-center items-center cursor-pointer"
-                  }
-                  isOpen={isModalOpen}
-                  onClose={toggleModal}
-                  onOpen={toggleModal}
-                  Icon={<HiOutlineCamera color="white" size={30} onClick={()=>toggleModal}/>}
-                  size="4xl"
-                  content={
-                    <div className="border  border-red-500 flex flex-col justify-between items-center h-[400px]">
-                      <div className="w-[200px] h-[200px] relative border ">
-                        <img src={profPic} className="border w-full h-full" />
-                        <button className="w-[50px] h-[50px] bg-dark-purple absolute -right-2 -bottom-2 rounded-full flex-center cursor-pointer" >
-                          <FaCheck size={25} className="text-white" onClick={()=>setIsModalOpen(false)} />
-
-                        </button>
-                      </div>
-                      <div className="w-full border border-amber-900">
-                        <div className="flex gap-5 items-center">
-                          <div className="flex gap-5">
-                            <img
-                              src={profPicccccccc}
-                              className="border w-[100px] h-[100px]"
-                            />
-                            <img
-                              src={profPicccccccc}
-                              className="w-[100px] h-[100px]"
-                            />
-                            <img
-                              src={profPicccccccc}
-                              className="w-[100px] h-[100px]"
-                            />
-                          </div>
-                          <div>
-                            <label htmlFor="formFile">
-                              <FaPlus size={30} cursor={"pointer"} className="text-dark-purple"/>
-                            </label>
-                            <Field
-                              type="file"
-                              id="formFile"
-                              name="formFile"
-                              accept="image/png, image/jpeg"
-                              className="hidden"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  }
-                />
-              </div>
+            <ImageContainer currentProf={currentProf}/>
 
               <FormGroup
                 name={"UserAbout"}
