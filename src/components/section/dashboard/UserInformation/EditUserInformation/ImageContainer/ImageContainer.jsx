e55a62de70235ae90a965a0f55125947385a4249
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-11-07 04:51:27 +0330

    Update ImageContainer.jsx

--- src/components/section/dashboard/UserInformation/EditUserInformation/ImageContainer/ImageContainer.jsx
+++ src/components/section/dashboard/UserInformation/EditUserInformation/ImageContainer/ImageContainer.jsx
@@ -1,47 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 // import UserProfModal from '../UserProfModal/UserProfModal'
 import ModalSection from "../../../../../ui/Modal/ModalSection";
 import { Field } from "formik";
 import { FaCheck } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
-import profPic from "../../../../../../assets/icons/ax-kartoni-bamazeh-11.svg";
-import profPicccccccc from "../../../../../../assets/images/prof.png";
 import { HiOutlineCamera } from "react-icons/hi2";
 import { IoClose } from "react-icons/io5";
 import useToggle from "../../../../../../hooks/useToggle";
 import toast from "react-hot-toast";
-import { useMutation } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
 import { postUserImage } from "../../../../../../servises/api/userPanel/addProfileImage";
 import { postUserImageSelect } from "../../../../../../servises/api/userPanel/selectProfileImage";
 
 const ImageContainer = ({ currentProf }) => {
   const [isModalOpen, toggleModal, setIsModalOpen] = useToggle(false);
+  const [selectedImg, setSelectedImg] = useState(null);
+  const queryClient = useQueryClient();
 
   const mutationImage = useMutation({
     mutationFn: (formData) => postUserImage(formData),
-    onError: (error) => {
-      console.log("خطا رخ داد", error);
-    },
+    onError: () => toast.error("خطا در آپلود عکس"),
     onSuccess: () => {
       toast.success("پروفایل شما اضافه شد");
+      queryClient.invalidateQueries(["currentProfUser"])
     },
   });
+
   const mutationImageSelect = useMutation({
     mutationFn: (formData) => postUserImageSelect(formData),
-    onError: (error) => {
-      console.log("خطا رخ داد", error);
-    },
+    onError: () => toast.error("خطا در انتخاب عکس"),
     onSuccess: () => {
       toast.success("پروفایل شما انتخاب شد");
+      setIsModalOpen(false);
+      queryClient.invalidateQueries(["currentProfUser"])
+
     },
   });
 
   const currentImage = currentProf?.userImage;
 
-  // console.log("currentImage",currentImage?.puctureAddress)
   return (
     <div className=" relative overflow-hidden  rounded-full w-[150px] h-[150px]">
-      <img src={currentProf?.userPicture || profPicccccccc} alt="profile" />
+      <img src={currentProf?.currentPictureAddress} alt="profile" />
 
       <ModalSection
         StyleModal={
@@ -60,11 +60,18 @@ const ImageContainer = ({ currentProf }) => {
         size="4xl"
         content={
           <div className="flex flex-col justify-between items-center h-[400px] px-8">
-            <div className="w-[200px] h-[200px] relative">
-              <img src={profPic} className="w-full h-full" />
+            <div className="w-[200px] h-[200px] relative  rounded-5">
+              <img
+                src={selectedImg?.puctureAddress}
+                className="w-full h-full  rounded-2xl"
+              />
               <button
                 className="w-[50px] h-[50px] bg-dark-purple absolute -right-2 -bottom-2 rounded-full flex-center cursor-pointer"
-                onClick={() => setIsModalOpen(false)}
+                onClick={() => {
+                  const formData = new FormData();
+                  formData.append("ImageId", selectedImg.id);
+                  mutationImageSelect.mutate(formData);
+                }}
               >
                 <FaCheck size={25} className="text-white" />
               </button>
@@ -72,15 +79,21 @@ const ImageContainer = ({ currentProf }) => {
             <div className="w-full">
               <div className="flex gap-5 items-center ">
                 {currentImage?.map((img, index) => (
-                <div className="border relative">
+                  <div className=" relative" key={img.id}>
                     <img
-                    key={img.id}
-                    src={img.puctureAddress}
-                    alt={`image ${index}`}
-                    className="w-[100px] h-[100px] object-cover rounded"
-                  />
-                  <button className=" absolute -top-3 -left-3 w-[25px] h-[25px] flex-center rounded-full bg-dark-purple"><IoClose color="white"/></button>
-                </div>
+                      src={img.puctureAddress}
+                      alt={`image ${index}`}
+                        className={`w-[100px] h-[100px] object-cover rounded cursor-pointer transition ${
+                        selectedImg?.id === img.id ? "ring-4 ring-dark-purple" : ""
+                      }`}
+                      onClick={() => {
+                        setSelectedImg(img);
+                      }}
+                    />
+                    <button className=" absolute -top-3 -left-3 w-[25px] h-[25px] flex-center rounded-full bg-dark-purple">
+                      <IoClose color="white" />
+                    </button>
+                  </div>
                 ))}
                 <div className="flex gap-5"></div>
                 <div>
