ae038ac6bbe2ef7df04a55d68f233b7bb23b64aa
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-11-13 03:38:11 +0330

    Update CompareCoursePage.jsx

--- src/pages/CompareCoursePage/CompareCoursePage.jsx
+++ src/pages/CompareCoursePage/CompareCoursePage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import MapComponent from '../../components/ui/Map/MapComponent'
 
 const CompareCoursePage = () => {
   return (
-    <div className='w-[300px] h-[300px]'>
+    <div>
       CompareCoursePage
-      <MapComponent/>
     </div>
   )
 }
