642f2e3e1730cffd91d30a5907d66f580eb49538
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-01 18:32:59 +0330

    comment like and dislike

--- src/components/section/CourseDetail/CourseComment/CourseComment.jsx
+++ src/components/section/CourseDetail/CourseComment/CourseComment.jsx
@@ -1,52 +1,119 @@
-import React from 'react'
-import CommentForm from './Form/CommentForm';
-import { useParams } from 'react-router-dom';
-import { useQuery} from '@tanstack/react-query';
-import { getCourseDetailComment } from '../../../../servises/api/coursesDetail/getComment';
-import UserComment from './UserComment/UserComment';
-const CourseComment = ({ teacherName, imageAddress}) => {
-    const { id } = useParams();
-    const { data } = useQuery({
-        queryKey: ['comment', id],
-        queryFn:getCourseDetailComment,
-    });
-    console.log("داده", id)
-    
-    return (
-        <div className=' border-[#DFDFDF] flex justify-center items-center'>
-            <div className=' flex flex-col items-end gap-5'>
-                <div className='w-full h-[140px] flex items-center lg:gap-4 bg-[#F7F7FA] rounded-[10px]'>
-                    <div className ='flex flex-col items-end mr-[10px]'>
-                        <h3 className='font-[400] text-[9px] sm:text-[12px] lg:text-[16px] text-[#1C1A4A]'>نویسنده</h3>
-                        <h4 className='font-[500] text-[14px] sm:text-[18px] lg:text-[22px] text-[#1C1A4A]'>{teacherName}</h4>
-                        <h2 className=' text-right font-[400] w-[70%] text-[6px] sm:text-[9px] lg:text-[14px]'>مهندس حسین نوری کادیجانی دارای مدرک کارشناسی مهندسی کامپیوتر – نرم‌افزار از دانشگاه تربیت دبیر شهید رجایی هستند. ایشان شش سال سابقه برنامه‌نویسی اندروید دارند و تاکنون برنامه‌های موفق و باکیفیتی را در مارکت‌های مطرح ایرانی منتشر کرده‌اند و پیوسته به دنبال یادگیری و کسب دانش بیشتر در این زمینه هستند.</h2>
-                    </div>
-                    <img src={imageAddress||"/default.png"} className='lg:w-[20%] sm:w-[75%] w-[70%] mr-[25px]' />
-                </div>
-                <p className='font-[600] text-[14px] sm:text-[18px] lg:text-[22px] mr-5'>نظر </p>
-                {data?.map(item =>
-                    <UserComment
-                        key={item.id}
-                        insertDate={item.insertDate.slice(0, 10)}
-                        author={item.author}
-                        disslikeCount={item.disslikeCount}
-                        likeCount={item.likeCount}
-                        describe={item.describe}
-                        title={item.title}
-                        pictureAddress={item.pictureAddress}
-                        id={item.id}
-                        CourseId={id}
-                        CourseCommandId={item.CourseCommandId}
-                    />
-                )}
-                
+import React from "react";
+import CommentForm from "./Form/CommentForm";
+import { useParams } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getCourseDetailComment } from "../../../../servises/api/coursesDetail/getComment";
+import UserComment from "./UserComment/UserComment";
+import {
+  PostCommentLike,
+  PostDeleteCommentLike,
+} from "../../../../servises/api/coursesDetail/PostCommentLike";
+import toast from "react-hot-toast";
+import { PostCommentDisLike } from "../../../../servises/api/coursesDetail/PostCommentDisLike";
+const CourseComment = ({ teacherName, imageAddress }) => {
+  const { id } = useParams();
+  const { data } = useQuery({
+    queryKey: ["comment", id],
+    queryFn: () => getCourseDetailComment(id),
+  });
+  const queryClient = useQueryClient();
+  const likeMutation = useMutation({
+    mutationFn: (commentId) => PostCommentLike(commentId),
+    onError: (error) => {
+      const msg = error?.response?.data?.message;
+      toast.error(msg);
+      console.error(error);
+    },
+    onSuccess: () => {
+      toast.success("کامنت لایک شد");
+      queryClient.invalidateQueries(["comment", id]);
+    },
+  });
+  const deleteLikeMutation = useMutation({
+    mutationFn: (commentId) => PostDeleteCommentLike(commentId),
+    onError: (error) => {
+      const msg = error?.response?.data?.message;
+      toast.error(msg);
+      console.error(error);
+    },
+    onSuccess: () => {
+      toast.success("لایک کامنت حذف شد");
+      queryClient.invalidateQueries(["comment", id]);
+    },
+  });
+  const disLikeMutation = useMutation({
+    mutationFn: (commentId) => PostCommentDisLike(commentId),
+    onError: (error) => {
+      console.error(error);
+      const msg = error?.response?.data?.message;
+      toast.error(msg);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["comment", id]);
+      toast.success("کامنت دیسلایک شد");
+    },
+  });
 
-                <div className=' w-full h-[270px] md:h-[390px] bg-[#F7F7FA] rounded-[10px] flex justify-center items-center'>
-                    <CommentForm/>
-                </div>
-            </div>
+  return (
+    <div className=" border-[#DFDFDF] flex justify-center items-center">
+      <div className=" flex flex-col items-end gap-5">
+        <div className="w-full h-[140px] flex items-center lg:gap-4 bg-[#F7F7FA] rounded-[10px]">
+          <div className="flex flex-col items-end mr-[10px]">
+            <h3 className="font-[400] text-[9px] sm:text-[12px] lg:text-[16px] text-[#1C1A4A]">
+              نویسنده
+            </h3>
+            <h4 className="font-[500] text-[14px] sm:text-[18px] lg:text-[22px] text-[#1C1A4A]">
+              {teacherName}
+            </h4>
+            <h2 className=" text-right font-[400] w-[70%] text-[6px] sm:text-[9px] lg:text-[14px]">
+              مهندس حسین نوری کادیجانی دارای مدرک کارشناسی مهندسی کامپیوتر –
+              نرم‌افزار از دانشگاه تربیت دبیر شهید رجایی هستند. ایشان شش سال
+              سابقه برنامه‌نویسی اندروید دارند و تاکنون برنامه‌های موفق و
+              باکیفیتی را در مارکت‌های مطرح ایرانی منتشر کرده‌اند و پیوسته به
+              دنبال یادگیری و کسب دانش بیشتر در این زمینه هستند.
+            </h2>
+          </div>
+          <img
+            src={imageAddress || "/default.png"}
+            className="lg:w-[20%] sm:w-[75%] w-[70%] mr-[25px]"
+          />
         </div>
-    );
+        <p className="font-[600] text-[14px] sm:text-[18px] lg:text-[22px] mr-5">
+          نظر{" "}
+        </p>
+        {data?.map((item) => {
+          console.log("idddddd", item.id);
+          return (
+            <UserComment
+              key={item.id}
+              insertDate={item.insertDate.slice(0, 10)}
+              author={item.author}
+              disslikeCount={item.disslikeCount}
+              likeCount={item.likeCount}
+              describe={item.describe}
+              title={item.title}
+              pictureAddress={item.pictureAddress}
+              id={item.id}
+              currentUserIsLike={item.currentUserIsLike}
+              currentUserIsDissLike={item.currentUserIsDissLike}
+              onLike={() => {
+                item.currentUserIsLike
+                  ? deleteLikeMutation.mutate(item.id)
+                  : likeMutation.mutate(item.id);
+              }}
+              onDislike={() => {
+                disLikeMutation.mutate(item.id);
+              }}
+            />
+          );
+        })}
+
+        <div className=" w-full h-[270px] md:h-[390px] bg-[#F7F7FA] rounded-[10px] flex justify-center items-center">
+          <CommentForm />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default CourseComment
+export default CourseComment;
--- src/components/section/CourseDetail/CourseComment/UserComment/UserComment.jsx
+++ src/components/section/CourseDetail/CourseComment/UserComment/UserComment.jsx
@@ -1,128 +1,107 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { BiLike, BiDislike } from "react-icons/bi";
-import GetReply from './GetReply';
-import Tag from '../../../../ui/Tag/Tag';
-import { AiFillLike, AiOutlineLike } from 'react-icons/ai';
-import ModalSection from '../../../../ui/Modal/ModalSection';
-import useToggle from '../../../../../hooks/useToggle';
-import PostReply from './PostReply';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { PostCommentLike } from '../../../../../servises/api/coursesDetail/PostCommentLike';
-import { PostCommentDisLike } from '../../../../../servises/api/coursesDetail/PostCommentDisLike';
-import toast from 'react-hot-toast';
-
-const UserComment = ({ insertDate, author, disslikeCount, currentUserIsLike, currentUserIsDissLike, likeCount, CourseCommandId, pictureAddress, describe, title, id, }) => {
-    const [showReplies, setShowReplies] = useState(false);
-    
-    const [isModalOpen, toggleModal, setIsModalOpen] = useToggle(false);
-    
-    const queryClient = useQueryClient();
-    const likeMutation = useMutation({
-        mutationFn :()=> PostCommentLike(CourseCommandId),
-        onError : (error) => {
-            console.error(error)
-        },
-        onSuccess : () => {
-            queryClient.invalidateQueries(["comment"]);
-            toast.success("کامنت لایک شد")
-        },       
-    })
-    const disLikeMutation = useMutation({
-        mutationFn :()=> PostCommentDisLike(CourseCommandId),
-        onError : (error) => {
-            console.error(error)
-        },
-        onSuccess : () => {
-            queryClient.invalidateQueries(["comment"]);
-            toast.success("کامنت دیسلایک شد")
-        },       
-    })
-    console.log("CourseCommandId:", CourseCommandId);
-
-    
-    const toggleReplies = async () => {
-        setShowReplies(prev => !prev)
-    }
-    console.log("commentcouurseID", CourseCommandId)
+import GetReply from "./GetReply";
+import Tag from "../../../../ui/Tag/Tag";
+import { AiFillLike, AiOutlineLike } from "react-icons/ai";
+import ModalSection from "../../../../ui/Modal/ModalSection";
+import useToggle from "../../../../../hooks/useToggle";
+import PostReply from "./PostReply";
 
 
-    return (
-        <>
-            <div className='w-full border-t-1 border-[#E8E8E8] h-[120px] lg:h-[140px] flex gap-4'>
-                <div className='flex w-full flex-col items-end mt-[10px] lg:mt-[8px] gap-1 lg:gap-2'>
-                    <div className='w-full flex justify-between'>
-                        <h1 className=' text-right font-[400] text-[7px] sm:text-[9px] lg:text-[12px] text-[#6D6C80] ml-5'>{insertDate} </h1>
-                        <h2 className='font-[600] text-[12px] text-[#1C1A4A]'>{author}</h2>
-                    </div>
+const UserComment = ({
+  insertDate,
+  author,
+  disslikeCount,
+  currentUserIsLike,
+  currentUserIsDissLike,
+  likeCount,
+  pictureAddress,
+  describe,
+  title,
+  id,
+  onLike,
+  onDislike,
+}) => {
+  const [showReplies, setShowReplies] = useState(false);
 
-                    <h3 className='font-[600] text-[11px] sm:text-[14px] lg:text-[16px] text-[#1C1A4A]'> {title}</h3>
-                    <h4 className=' text-right font-[400] text-[8px] sm:text-[10px] lg:text-[14px] text-[#6D6C80]'>{describe}</h4>
-                    <div className='flex w-full justify-between'>
-                        <div className='w-[85px] h-[25px] flex flex-row gap-0.5'>
-                            <div className='w-[45%] md:w-[55%] h-full rounded-[15px] bg-soft-gray font-[500] text-[10px] md:text-[12px] flex flex-row items-center gap-0.5 justify-center text-[#5F5F66]' > 
-                                <Tag icon={
-                                currentUserIsLike ? (
-                                    <AiFillLike size={"20px"} className="text-gray-500" />
-                                ) : (
-                                    <AiOutlineLike size={"20px"} />
-                                )
-                            }
-                                onClick={()=>{likeMutation.mutate(CourseCommandId)
-                                    console.log("like CourseCommandId:", CourseCommandId)}}
-                                title={likeCount}
-                            /></div>
-                            <div className='w-[45%] md:w-[55%] h-full rounded-[15px] bg-soft-gray font-[500] text-[10px] md:text-[12px] flex flex-row items-center gap-0.5 justify-center text-[#5F5F66]' > 
-                                <Tag icon={
-                                currentUserIsDissLike ? (
-                                    <BiDislike size={"20px"} className="text-gray-500" />
-                                ) : (
-                                    <BiDislike size={"20px"} />
-                                )
-                            }
-                            onClick={()=>disLikeMutation.mutate(CourseCommandId)}
+  const [isModalOpen, toggleModal, setIsModalOpen] = useToggle(false);
 
-                                title={disslikeCount}
-                            />
-                            </div>
-                        </div>
-                        <div className=' h-[25px] flex flex-row gap-0.5'>
-                            <button className ='lg:px-3 text-dark-purple h-[34px] text-[8px] sm:text-[10px] lg:text-[12px] bg-light-purple sm:h-[30px] sm:px-2 px-1.5 rounded-[10px] flex items-center'
-                                onClick={toggleReplies}
-                            >
-                                {showReplies ? " بستن  پاسخ ها" : " مشاهده پاسخ ها"}
-                            </button>
-                            <ModalSection
-                                StyleModal={"lg:px-3 sm:px-2 h-[30px] rounded-[10px] flex items-center bg-light-purple"}
-                                isOpen={isModalOpen}
-                                onClose={toggleModal}
-                                onOpen={toggleModal}
-                                size="3xl"
-                                content={
-                                    <div className ="flex flex-col justify-between items-center h-[300px] bg-[#F7F7FA] px-8">
-                                        <PostReply
-                                            parentCommentId = {id}
-                                        />
-                                    </div>
-                                }
-                                ButtonText={ 
-                                    <span className='font-[500] text-[8px] sm:text-[10px] lg:text-[12px] text-dark-purple'>پاسخ</span>
-                                }
-                            >
-                            
-                            </ModalSection>
-                        </div>
-                    </div>
-                </div>
-                <img src={pictureAddress||"/default.png"} className='lg:w-[12%] lg:h-[55%] w-[17%] h-[40%] rounded-full mt-[20px]' />
+  const toggleReplies = async () => {
+    setShowReplies((prev) => !prev);
+  };
 
-            </div>
-            {showReplies && (
-                <GetReply
-                    parentCommentId={id}
+  return (
+    <>
+      <div className="w-full border-t-1 border-[#E8E8E8] h-[120px] lg:h-[140px] flex gap-4">
+        <div className="flex w-full flex-col items-end mt-[10px] lg:mt-[8px] gap-1 lg:gap-2">
+          <div className="w-full flex justify-between">
+            <h1 className=" text-right font-[400] text-[7px] sm:text-[9px] lg:text-[12px] text-[#6D6C80] ml-5">
+              {insertDate}{" "}
+            </h1>
+            <h2 className="font-[600] text-[12px] text-[#1C1A4A]">{author}</h2>
+          </div>
+
+          <h3 className="font-[600] text-[11px] sm:text-[14px] lg:text-[16px] text-[#1C1A4A]">
+            {" "}
+            {title}
+          </h3>
+          <h4 className=" text-right font-[400] text-[8px] sm:text-[10px] lg:text-[14px] text-[#6D6C80]">
+            {describe}
+          </h4>
+          <div className="flex w-full justify-between">
+            <div className="w-[85px] h-[25px] flex flex-row gap-0.5">
+              <div className="w-[45%] md:w-[55%] h-full rounded-[15px] bg-soft-gray font-[500] text-[10px] md:text-[12px] flex flex-row items-center gap-0.5 justify-center text-[#5F5F66]">
+                <Tag
+                  icon={currentUserIsLike ? <AiFillLike /> : <AiOutlineLike />}
+                  onClick={onLike}
+                  title={likeCount}
                 />
-            )}
-        </>
-    )
-}
+              </div>
+              <div className="w-[45%] md:w-[55%] h-full rounded-[15px] bg-soft-gray font-[500] text-[10px] md:text-[12px] flex flex-row items-center gap-0.5 justify-center text-[#5F5F66]">
+                <Tag
+                  icon={currentUserIsDissLike ? <BiDislike /> : <BiDislike />}
+                  onClick={onDislike}
+                  title={disslikeCount}
+                />
+              </div>
+            </div>
+            <div className=" h-[25px] flex flex-row gap-0.5">
+              <button
+                className="lg:px-3 text-dark-purple h-[34px] text-[8px] sm:text-[10px] lg:text-[12px] bg-light-purple sm:h-[30px] sm:px-2 px-1.5 rounded-[10px] flex items-center"
+                onClick={toggleReplies}
+              >
+                {showReplies ? " بستن  پاسخ ها" : " مشاهده پاسخ ها"}
+              </button>
+              <ModalSection
+                StyleModal={
+                  "lg:px-3 sm:px-2 h-[30px] rounded-[10px] flex items-center bg-light-purple"
+                }
+                isOpen={isModalOpen}
+                onClose={toggleModal}
+                onOpen={toggleModal}
+                size="3xl"
+                content={
+                  <div className="flex flex-col justify-between items-center h-[300px] bg-[#F7F7FA] px-8">
+                    <PostReply parentCommentId={id} />
+                  </div>
+                }
+                ButtonText={
+                  <span className="font-[500] text-[8px] sm:text-[10px] lg:text-[12px] text-dark-purple">
+                    پاسخ
+                  </span>
+                }
+              ></ModalSection>
+            </div>
+          </div>
+        </div>
+        <img
+          src={pictureAddress || "/default.png"}
+          className="lg:w-[12%] lg:h-[55%] w-[17%] h-[40%] rounded-full mt-[20px]"
+        />
+      </div>
+      {showReplies && <GetReply parentCommentId={id} />}
+    </>
+  );
+};
 
-export default UserComment
+export default UserComment;
