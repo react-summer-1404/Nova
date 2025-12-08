7e4f134c9546f45fab279df5eb25912d3093d4f6
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-08 09:31:19 +0330

    .

--- src/components/ui/Logo/Logo.jsx
+++ src/components/ui/Logo/Logo.jsx
@@ -8,7 +8,6 @@ const Logo = () => {
           <span className ="font-bold xl:text-xl md:text-base hidden md:block text-black">نامبروان</span>
           <span className ="text-xs whitespace-nowrap hidden md:block text-black">همیشه بهترین</span>
           </div>
-          <img src="/logo.svg" alt="Logo" className ="filter drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] xl:w-[50px] sm:w-[50px] w-[35px]"></img>
     </Link>
         
   )
--- src/features/determineLevel/DetermineLevel.jsx
+++ src/features/determineLevel/DetermineLevel.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import {motion} from "framer-motion";
+
 
 const DetermineLevel = () => {
+    const text = `سلام! ما تیم نامبر وان هستیم 🚀
+    خیلی خوش اومدی به بخش تعیین سطح برنامه‌نویسی.
+    اینجا فقط با چند تا سؤال کوتاه، می‌فهمیم سطح فعلی‌ت چیه تا بهترین مسیر یادگیری رو مخصوص خودت پیشنهاد بدیم.
+    این مرحله خیلی سریع و آسونه، ولی کمک بزرگی می‌کنه که دقیقاً بدونی از کجا باید شروع کنی.
+   آماده‌ای شروع کنیم؟`;
+    
+    const LETTER_DELAY = 0.025;    
+  
   return (
-    <div>
-      sdfghj
+    <div className=" w-screen flex-center bg-light-purple h-screen">
+      <div className="w-[60%]   ">
+        <div className="flex-center">
+          <div className="flex items-center gap-3 p-4 ">
+            <div className="flex items-center justify-end ">
+              <div className="bg-white p-4 my-6 rounded-lg flex-1 ">
+              <span className="ml-3">
+             
+              {text.split("").map((char, i) => (
+                <motion.span
+                  key={i}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{
+                    delay: i * LETTER_DELAY,
+                    duration: 0.2,
+                    ease: "easeInOut",
+                  }}
+                >
+                  {char}
+                </motion.span>
+              ))}
+            </span>
+              </div>
+              <div className="w-3 overflow-hidden">
+                <div className="h-4 bg-white rotate-45 transform origin-top-left rounded-sm"></div>
+              </div>
+            </div>
+            <img
+              src="/logo.svg"
+              alt="Logo"
+              className="filter drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] xl:w-[50px] sm:w-[50px] w-[35px]"
+            ></img>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DetermineLevel
+export default DetermineLevel;
