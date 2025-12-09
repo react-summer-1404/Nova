fa5df2129eb9be99247d32da71b168acbf46df62
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-09 09:16:48 +0330

    .

--- src/components/section/CompareCourse/CoursesInfo.jsx
+++ src/components/section/CompareCourse/CoursesInfo.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 import { AiFillAlert } from "react-icons/ai";
 import { useQuery } from "@tanstack/react-query";
-import { getCourseDetail } from '../../../servises/api/coursesDetail/getDetail';
+import { getCourseDetail } from "../../../servises/api/coursesDetail/getDetail";
 
 const CoursesInfo = ({ cId }) => {
-
   const { data } = useQuery({
     queryKey: ["getCourseDetailId", cId],
     queryFn: () => getCourseDetail(cId),
@@ -12,13 +11,27 @@ const CoursesInfo = ({ cId }) => {
   });
 
   const options = [
-    { name: "تاریخ", content: "22-02-2205", icon: <AiFillAlert /> },
-    { name: "تاریخ", content: "22-02-2205", icon: <AiFillAlert /> },
+    { name: ":نام استاد", content: data?.teacherName, icon: <AiFillAlert /> },
+    {
+      name: ":تاریخ شروع",
+      content: data?.startTime?.slice(0, 10),
+      icon: <AiFillAlert />,
+    },
+    { name: ":ظرفیت", content: data?.capacity, icon: <AiFillAlert /> },
+    {
+      name: ":سطح دوره",
+      content: data?.courseLevelName,
+      icon: <AiFillAlert />,
+    },
+    {
+      name: ":توضیحات کوتاه ",
+      content: data?.miniDescribe,
+      icon: <AiFillAlert />,
+    },
   ];
 
   return (
     <div className="flex flex-col items-center justify-center relative h-[800px]">
-
       <div className="w-[350px] h-[200px] absolute top-2 shadow-[5px_5px_10px_0px_rgba(0,0,0,0.25)] rounded-[25px]">
         <img
           className="w-[350px] h-[200px] rounded-[25px]"
@@ -28,7 +41,9 @@ const CoursesInfo = ({ cId }) => {
 
       <div className="bg-light-purple w-[450px] h-[70%] flex items-center justify-center rounded-[25px] border-2 border-[#5751E133]">
         <div className="flex flex-col gap-8 w-[90%]">
-          <h2>{data?.title || "در حال بارگذاری..."}</h2>
+          <h2 className="font-semibold text-black text-lg">
+            {data?.title || "در حال بارگذاری..."}
+          </h2>
 
           <div className="flex flex-col text-dark-purple gap-8">
             {options.map((option, index) => (
@@ -41,10 +56,8 @@ const CoursesInfo = ({ cId }) => {
               </div>
             ))}
           </div>
-
         </div>
       </div>
-
     </div>
   );
 };
--- src/pages/CompareCoursePage/CompareCoursePage.jsx
+++ src/pages/CompareCoursePage/CompareCoursePage.jsx
@@ -3,7 +3,7 @@ import { useSearchParams } from "react-router-dom";
 import NavigationSection from "../../components/ui/navigation/NavigationSection";
 import TopSection from "../../components/section/CompareCourse/topSection";
 import CoursesInfo from "../../components/section/CompareCourse/coursesInfo";
-import vs from "../../assets/images/vs.svg"
+import vs from "../../assets/images/vs.svg";
 const CompareCoursePage = () => {
   const [searchParams] = useSearchParams();
   const courseIds = searchParams.getAll("courseId");
@@ -12,12 +12,14 @@ const CompareCoursePage = () => {
     <div className="flex flex-col   w-screen  justify-center ">
       <NavigationSection title={"مقایسه دو دوره"} />
       <TopSection />
-      <div className="flex flex-col gap-5 p-8">
-        <div className="flex">
-        <CoursesInfo cId={courseIds[0]} />
-          <img src={vs}/>
+      <div className="flex-center flex-col gap-5 p-8">
+        <div className="flex justify-between w-[80%]">
+          <CoursesInfo cId={courseIds[0]} />
+          <div className="h-[300px]  flex-center">
+          <img className="w-[200px] h-[100px] pt-6 " src={vs} />
+
+          </div>
           <CoursesInfo cId={courseIds[1]} />
-          <div></div>
         </div>
       </div>
     </div>
