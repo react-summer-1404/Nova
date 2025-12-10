5d0dbad0d922fae48a9edd7e15db69be5ec17c93
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-10 21:59:23 +0330

    landing chamged

--- src/components/section/aboutUs/AboutMore.jsx
+++ src/components/section/aboutUs/AboutMore.jsx
@@ -1,35 +1,58 @@
-import React from 'react'
-import aboutPic from "../../../assets/images/Section → inner_about_img.svg"
-import svg from "../../../assets/icons/Section → SVG.svg"
-import Tag from '../../../components/ui/Tag/Tag'
-import { AccentText } from '../../../components/ui'
-import AboutOption from '../../../components/section/landing/AboutSection/AboutOption'
-import BlueButton from "../../../components/ui/button/BlueButton"
+import React from "react";
+import aboutPic from "../../../assets/images/Section → inner_about_img.svg";
+import svg from "../../../assets/icons/Section → SVG.svg";
+import Tag from "../../../components/ui/Tag/Tag";
+import { AccentText } from "../../../components/ui";
+import AboutOption from "../../../components/section/landing/AboutSection/AboutOption";
+import BlueButton from "../../../components/ui/button/BlueButton";
 import { GoArrowLeft } from "react-icons/go";
+import yellowLable from "../../../assets/images/Group 2.svg";
 const AboutMore = () => {
   return (
     <div className="flex flex-col gap-8  w-screen items-center">
-         <div className='flex md:w-fit lg:p-[132px] p-8 gap-[70px]  w-[90%]'>
-    <div className='flex flex-col items-end gap-5 md:w-[650px] w-full  '>
-     <Tag title={"درباره ما بیشتر بدانید"} textColor={"#5751E1"} bgColor={"#EFEEFE"} width={"177px"} height={"31px"}/>
-     <div className='hidden lg:block'>
-     <AccentText beforeLabel={"توانسازی دانش اموزان برای رسیدن به هدف"} afterLabel={"انها برای چالش سطح بعدی"} title={"بالقوه"}/>
+      <div className="flex md:w-fit lg:p-[132px] p-8 gap-[70px]  w-[90%]">
+        <div className="flex flex-col items-end gap-5 md:w-[650px] w-full  ">
+          <Tag
+            title={"درباره ما بیشتر بدانید"}
+            textColor={"#5751E1"}
+            bgColor={"#EFEEFE"}
+            width={"177px"}
+            height={"31px"}
+          />
+          <div className="hidden lg:block">
+            <div
+              className="flex flex-col-reverse font-bold xl:text-4xl lg:text-2xl text-black text-base  text-right gap-3  whitespace-nowrap "
+              dir="rtl"
+            >
+              <p>سطح بعدی</p>
 
-     </div>
-     <span style={{color:"var(--color-text-gray)"}} className='text-right'>سلام! این یک پیام تستی است سلام! این یک پیام تستی است سلام! این یک پیام تستی است سلام! این یک پیام تستی است سلام! این یک پیام تستی است سلام! این یک پیام تستی است سلام! این یک پیام تستی است </span>
-     <AboutOption/>
-     <BlueButton content={"درباره ما"} BtnIcon={<GoArrowLeft />} />
-     </div>
+              <div className="flex items-center gap-2">
+                <p>هدف</p>
+                <img src={yellowLable} className="lg:w-[190px]  w-[100px]"/>
+                <p>انها برای چالش </p>
+              </div>
+              <p>توانسازی داشن اموزان برای رسیدن به </p>
+            </div>
+          </div>
+          <span
+            style={{ color: "var(--color-text-gray)" }}
+            className="text-right"
+          >
+            سلام! این یک پیام تستی است سلام! این یک پیام تستی است سلام! این یک
+            پیام تستی است سلام! این یک پیام تستی است سلام! این یک پیام تستی است
+            سلام! این یک پیام تستی است سلام! این یک پیام تستی است{" "}
+          </span>
+          <AboutOption />
+          <BlueButton content={"درباره ما"} BtnIcon={<GoArrowLeft />} />
+        </div>
 
-    <div className='lg:flex  w-fit relative hidden  h-fit'>
-       <img src={aboutPic}/>
-       <img src={svg} className='absolute top-0 right-0'/>
-     </div>
-
+        <div className="lg:flex  w-fit relative hidden  h-fit">
+          <img src={aboutPic} />
+          <img src={svg} className="absolute top-0 right-0" />
+        </div>
+      </div>
     </div>
-     </div>
-   
-  )
-}
+  );
+};
 
-export default AboutMore
+export default AboutMore;
--- src/components/section/landing/JoinTheClub/JoinTheClub.jsx
+++ src/components/section/landing/JoinTheClub/JoinTheClub.jsx
@@ -1,44 +1,61 @@
-import React from 'react'
+import React from "react";
 import { motion } from "framer-motion";
-import { scrollVariant } from '../../../../configs/frameMorion/Scroll';
-
+import { scrollVariant } from "../../../../configs/frameMorion/Scroll";
+import pic from "../../../../assets/images/landingPic.svg";
+import line from "../../../../assets/images/line.png";
+import YellowButton from "../../../ui/button/YellowButton";
+import Tag from "../../../ui/Tag/Tag";
+import { GoArrowLeft } from "react-icons/go";
 const JoinTheClub = () => {
   return (
-    <div className ='bg-dark-purple w-screen lg:h-[321px] h-[240px] flex sm:h-[280px]'>
-        <div className ='w-[28%] flex'>
-          
-          <div><img src="src/assets/images/line.png" className='h-full'/></div>
-          
-        </div>
-        
-      <motion.div className =' w-[35%] flex flex-col justify-center relative items-end gap-4'
-      variants={scrollVariant}
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: false }}>
-          <p className ='text-right text-[22px] text-[#fff] w-[100%] md:text-[34px] md:w-[70%]'>می خواهید از دوره های جدید در جریان باشید؟</p>
-          <input
-            type="text"
-            placeholder='ایمیل خود را تایپ کنید'
-            className ='lg:w-[80%] lg:h-[51px] h-[30px] sm:h-[33px] sm:w-[77%] w-[100%] bg-[#4A44D1] border border-[#433EC2] rounded-[50px] placeholder: lg:text-[14px] text-[8px] sm:text-[10px] lg:indent-60 sm:indent-16 indent-10 text-[#fff]'
+    <div className="bg-dark-purple w-screen  flex sm:h-[280px] flex-center">
+      <div className="md:w-[80%]  w-full  justify-end flex lg:h-[280px] relative items-center gap-4 ">
+      <div className="flex md:w-[50%] lg:w-[60%] w-full  items-center justify-between p-5 lg:flex-row flex-col-reverse gap-4">
+      <div className="w-[28%] flex  absolute left-0 bottom-0 h-full z-0 ">
+          <motion.img
+            src={line}
+            className="h-full hidden lg:block"
+            initial={{ x: -200 }}
+            whileInView={{ x: 0 }}
+            viewport={{ once: false, amount: 0.3 }}
+            transition={{ duration: 1 }}
           />
-          <button  className ='bg-golden-yellow lg:w-[35%] lg:h-[48px] absolute lg:bottom-[80px] sm:bottom-[81px] bottom-[65px] sm:h-[35px] sm:w-[47%] w-[50%] lg:right-[500px] right-[145px] sm:right-[210px] rounded-[50px] border border-[#141109] shadow-[4px_6px_0px_0px_#3D3D3D]'>
-            <p style={{color: "var(--color-blue-fot)"}}className ='text-[8px] sm:text-[10px] lg:text-[14px] font-[600]'> اکنون مشترک شوید </p>
-          </button>
+        </div>
+     
+        <div className="z-30">
+          <YellowButton
+            text={"همین حالا امتحان کنید"}
+            width={"200px"}
+            height={"35px"}
+            icon={<GoArrowLeft size={16}/>}
+          />
+        </div>
+        <motion.div
+          className="  flex flex-col justify-center relative items-center gap-4  "
+          variants={scrollVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: false }}
+        >
+          <Tag bgColor={"var(--color-light-purple)"} textColor={"var(--color-dark-purple)"} title={" ! تعیین سطح "} width={"200px"}/>
+          <p className="text-[22px] text-white md:w-[400px] w-[350px]  font-semibold">
+          با تعیین سطح می‌تونی دوره‌های مناسب خودت رو پیدا کنی و قوی‌تر از همیشه شروع کنی
+          </p>
+          
         </motion.div>
-      <motion.div className =' w-[22%] h-full'
-      variants={scrollVariant}
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: false }}>
-          <div className ='relative h-[280px] md:h-[300px]'>
-            <img src="src/assets/images/yellowCrescent.png" className ='absolute top-[213px] sm:top-[232px] lg:top-[219px] right-[-39px]'></img>
-            <img src="src/assets/images/pattern.png" className ='absolute lg:bottom-[85px] lg:left-[64px] left-[29px] bottom-[105px] sm:w-[20%] sm:h-[20%] w-[25%]' />
-            <img src="src/assets/images/man.png" className ='absolute sm:w-[100%] sm:h-[70%] sm:left-[60px] sm:bottom-[0px] lg:bottom-[-21px] bottom-[40px] h-[140px] right-[-30px] lg:left-[64px] lg:w-[67%] lg:h-[68%] sm:object-cover' />
+      </div>
+        <div className=" lg:w-[30%] w-[60%] lg:h-full h-[280px] overflow-visible hidden md:block ">
+          <div className="relative h-[280px] md:h-full  ">
+            <img
+              src={pic}
+              className=" absolute bottom-0 w-full"
+            
+            />
           </div>
-        </motion.div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default JoinTheClub
+export default JoinTheClub;
