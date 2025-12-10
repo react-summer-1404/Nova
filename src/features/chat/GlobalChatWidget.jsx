0fa4da6b1e83cc9cad3e20ebfb26879c027161f2
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2025-12-10 18:47:46 +0330

    Update GlobalChatWidget.jsx

--- src/features/chat/GlobalChatWidget.jsx
+++ src/features/chat/GlobalChatWidget.jsx
@@ -7,8 +7,6 @@ import ModalSection from "../../components/ui/Modal/ModalSection";
 import useToggle from "../../hooks/useToggle";
 
 export default function GlobalChatWidget() {
-  // const [open, setOpen] = useState(false); 
-  // const [isModalOpen, toggleModal, setIsModalOpen] = useToggle(false);
   const [isModalOpen, toggleModal, setIsModalOpen] = useToggle(false);
 
   return (
@@ -16,19 +14,17 @@ export default function GlobalChatWidget() {
       <div className="fixed bottom-6 right-6 z-50" >
       <ModalSection
         modalTitle="صفحه چت"
-        // Icon={FaRegMessage}
-        // style={{direction: "ltr"}}
         StyleModal={
-          "md:px-3 px-2 h-[30px] rounded-[10px] flex items-center bg-light-purple"
+          "w-[8px] h-[38px] bg-dark-purple dark:bg-Violet-Blue cursor-pointer transition-all ease-in-out"
         }
         isOpen={isModalOpen}
         onClose={toggleModal}
         onOpen={toggleModal}
         size="2xl"
         content={<Chat />}
-        Icon ={
-          <i className="w-[48px] h-[48px]  bg-dark-purple dark:bg-Violet-Blue cursor-pointer transition-all ease-in-out">{FaRegMessage}</i>
-        }
+        // Icon ={
+        //   <i className="w-[48px] h-[48px]  bg-dark-purple dark:bg-Violet-Blue cursor-pointer transition-all ease-in-out">{FaRegMessage}</i>
+        // }
       />
     </div>
     </>
