bde36997b3d0e19fef7483df3fa0757854fddcaa
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-11 03:35:54 +0330

    Update ContactUSPage.jsx

--- src/pages/contactUSPage/ContactUSPage.jsx
+++ src/pages/contactUSPage/ContactUSPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const ContactUsPage = () => {
   return (
     <div>
-      ContactUsPage
+      <div>
+        <div className="bg-base-gray w-5 h-8">hyrh</div>
+        <div></div>
+      </div>
+      <div>map</div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactUsPage
+export default ContactUsPage;
