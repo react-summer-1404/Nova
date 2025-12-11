9a72138a8a8135def5f1d7595fdcb44d2bea1aa7
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-11 13:36:05 +0330

    Update MedalSection.jsx

--- src/components/section/dashboard/UserDashboard/MedalSection.jsx
+++ src/components/section/dashboard/UserDashboard/MedalSection.jsx
@@ -7,24 +7,28 @@ const MedalSection = ({ data }) => {
   let medalImg = null;
   let medalText = "";
 
-  if(data >= 6) {
-    medalImg = third;
-    medalText = "عالیه! تو استاد شدی!";
-  } else if(data >= 3) {
-    medalImg = second;
-    medalText = "خوبه! داری کم کم حرفه‌ای میشی!";
-  } else if(data >= 1) {
+  if (data >= 6) {
     medalImg = first;
-    medalText = "آفرین داری خوب پیش میری، هنوز نوبی!";
+    medalText = "عالیه! تو استاد شدی!";
+  } else if (data >= 3) {
+    medalImg = second;
+    medalText = "!خوبه! داری کم کم حرفه‌ای میشی";
+  } else if (data >= 1) {
+    medalImg = third;
+    medalText = " !تازه شروع کردی ولی همین الان داری میدرخشی ";
   } else {
     medalImg = null;
-    medalText = "هنوز شروع نکردی، بیا جلوتر برو!";
+    medalText = "اولین مدالت اینجاست...  فقط کافیه یه دوره رو رزرو کنی";
   }
 
   return (
     <div className="flex  w-[80%] justify-between items-center p-3">
-      {medalImg && <img className="w-[200px] h-[140px]" src={medalImg} alt="Medal" />}
-      <p className="text-[#5B2C6F] font-bold text-3xl w-[300px]  ">{medalText}</p>
+      {medalImg && (
+        <img className="w-[200px] h-[140px]" src={medalImg} alt="Medal" />
+      )}
+      <p className="text-[#5B2C6F] font-bold text-3xl w-[300px]  ">
+        {medalText}
+      </p>
     </div>
   );
 };
