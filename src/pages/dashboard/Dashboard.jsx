77266e21a3fd481281a839dbeebbca2894389656
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-12 13:39:29 +0330

    .

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -9,32 +9,22 @@ import Dictaphone from "../../features/voiceAssistance/VoiceAssistance";
 import { IoNotifications } from "react-icons/io5";
 import { Badge, Button } from "@heroui/react";
 import { getNotification } from "../../servises/api/notificationApi";
+import NotifModal from "../../components/section/dashboard/UserDashboard/notifSection/NotifModal";
 const Dashboard = () => {
   const { data } = useQuery({
     queryKey: ["currentProfUsers"],
     queryFn: getCurrentUserProfile,
   });
-  const { data: notif } = useQuery({
-    queryKey: ["getNotification"],
-    queryFn: getNotification,
-  });
+
   return (
     <div className="w-screen h-screen flex-center  ">
       <div className="w-screen container-border flex justify-between ">
         <div className="flex flex-col w-[80%] h-screen bg-white rounded-l-[15px]  ">
           <div className="flex-center  lg:w-[95%] w-screen">
-            <div className="ml-6">
-            <Badge color="warning" content={notif?.length} shape="circle">
-              <Button isIconOnly radius="full" variant="light">
-                <IoNotifications
-                  size={30}
-                  className="cursor-pointer text-dark-purple"
-                />
-              </Button>
-            </Badge>
+            <div className="ml-6 mt-2">
+             <NotifModal/>
             </div>
             <Header data={data} />
-
           </div>
           <main className="px-8 py-2 w-screen lg:w-full ">
             <Outlet />
