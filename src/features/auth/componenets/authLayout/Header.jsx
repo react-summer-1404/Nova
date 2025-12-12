c2eb80da54696df70bb27a86a0df3f6c10ce197b
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-12 13:03:19 +0330

    .

--- src/features/auth/componenets/authLayout/Header.jsx
+++ src/features/auth/componenets/authLayout/Header.jsx
@@ -5,15 +5,9 @@ import DrawerComponent from "../../../../components/ui/Drawer/DrawerComponent";
 import SideBar from "../../../../components/section/dashboard/UserInformation/sideBar";
 import { MdMenu } from "react-icons/md";
 import { Link } from "react-router";
-import { IoNotifications } from "react-icons/io5";
-import { Badge, Button } from "@heroui/react";
-import { useQuery } from "@tanstack/react-query";
-import { getNotification } from "../../../../servises/api/notificationApi";
+
 const Header = ({ data }) => {
-  const { data: notif } = useQuery({
-    queryKey: ["getNotification"],
-    queryFn: getNotification,
-  });
+  
   return (
     <div className={"flex justify-between  w-full items-center"}>
       <div className="flex items-center justify-between p-8 gap-3">
@@ -21,20 +15,13 @@ const Header = ({ data }) => {
           <IoHome className="cursor-pointer text-dark-purple" size={30} />
         </Link>
 
-        <Badge color="warning" content={notif?.length} shape="circle">
-          <Button isIconOnly radius="full" variant="light">
-            <IoNotifications
-              size={30}
-              className="cursor-pointer text-dark-purple"
-            />
-          </Button>
-        </Badge>
+        
       </div>
 
-      <div className="lg:hidden">
+      <div className="lg:hidden ">
         <DrawerComponent
           buttonStyle={"bg-transparent"}
-          Icon={<MdMenu size={22} className="text-dark-purple" />}
+          Icon={<MdMenu size={30} className="text-dark-purple" />}
           Content={<SideBar data={data} />}
         />
       </div>
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,33 +1,55 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from './../../features/auth/componenets/authLayout/Header';
+import Header from "./../../features/auth/componenets/authLayout/Header";
 import { useQuery } from "@tanstack/react-query";
 import { getCurrentUserProfile } from "../../servises/api/userPanel/getProfileInfo";
-import DarkMode from './../../components/ui/darkMode/DarkMode';
+import DarkMode from "./../../components/ui/darkMode/DarkMode";
 import SideBar from "../../components/section/dashboard/UserInformation/SideBar";
 import Dictaphone from "../../features/voiceAssistance/VoiceAssistance";
-
+import { IoNotifications } from "react-icons/io5";
+import { Badge, Button } from "@heroui/react";
+import { getNotification } from "../../servises/api/notificationApi";
 const Dashboard = () => {
-  
   const { data } = useQuery({
     queryKey: ["currentProfUsers"],
-    queryFn:getCurrentUserProfile,
+    queryFn: getCurrentUserProfile,
+  });
+  const { data: notif } = useQuery({
+    queryKey: ["getNotification"],
+    queryFn: getNotification,
   });
   return (
     <div className="w-screen h-screen flex-center  ">
       <div className="w-screen container-border flex justify-between ">
-        <div className="flex flex-col w-[80%] h-screen bg-white rounded-l-[15px] ">
-          <div className="flex-left  w-screen lg:w-full "><Header data={data} /></div>
+        <div className="flex flex-col w-[80%] h-screen bg-white rounded-l-[15px]  ">
+          <div className="flex-center  lg:w-[95%] w-screen">
+            <div className="ml-6">
+            <Badge color="warning" content={notif?.length} shape="circle">
+              <Button isIconOnly radius="full" variant="light">
+                <IoNotifications
+                  size={30}
+                  className="cursor-pointer text-dark-purple"
+                />
+              </Button>
+            </Badge>
+            </div>
+            <Header data={data} />
+
+          </div>
           <main className="px-8 py-2 w-screen lg:w-full ">
             <Outlet />
           </main>
         </div>
-       <div className="hidden lg:block ">
-       <SideBar data={data}/>
-       </div>
+        <div className="hidden lg:block ">
+          <SideBar data={data} />
+        </div>
       </div>
-      <div><DarkMode /></div>
-      <div><Dictaphone /></div>
+      <div>
+        <DarkMode />
+      </div>
+      <div>
+        <Dictaphone />
+      </div>
     </div>
   );
 };
